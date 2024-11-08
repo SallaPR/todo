83f8590639dda9507be8a1a1b6f128082b59879a
SallaPR <salla.pr@hotmail.com>
2024-11-08 23:27:39 +0200

    part 6 done

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
-import todoRouter from "./routers/todoRouter.js";
 import dotenv from "dotenv";
+import todoRouter from "./routers/todoRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 dotenv.config();
+
 const port = process.env.PORT;
 
 const app = express();
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", todoRouter);
+app.use("/user", userRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
