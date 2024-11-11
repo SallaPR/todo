783e8b0019534d51015ed33537c0352984cd7a9f
SallaPR <salla.pr@hotmail.com>
2024-11-11 13:45:55 +0200

    part 8 done

--- server/index.test.js
+++ server/index.test.js
@@ -49,7 +49,22 @@ describe("POST task", () => {
       body: JSON.stringify({ description: null }),
     });
     const data = await response.json();
-    expect(response.status).to.equal(500);
+    expect(response.status).to.equal(400, data.error);
+    expect(data).to.be.an("object");
+    expect(data).to.include.all.keys("error");
+  });
+
+  it("should not post a task withg zero length description", async () => {
+    const response = await fetch(base_url + "/create", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify({ description: "" }),
+    });
+    const data = await response.json();
+    expect(response.status).to.equal(400, data.error);
     expect(data).to.be.an("object");
     expect(data).to.include.all.keys("error");
   });
@@ -90,6 +105,22 @@ describe("DELETE tasks", () => {
 describe("POST register", () => {
   const email = "register@foo.com";
   const password = "register123";
+
+  it("should not post a user with less than 8 character password", async () => {
+    const password = "short1";
+    const response = await fetch(base_url + "/user/register", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    });
+    const data = await response.json();
+    expect(response.status).to.equal(400, data.error);
+    expect(data).to.be.an("object");
+    expect(data).to.include.all.keys("error");
+  });
+
   it("should login with valid email and password", async () => {
     const response = await fetch(base_url + "/user/register", {
       method: "post",
